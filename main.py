53fe0a2ff6874bbb980ce74db8a89954c13d4b8c
kei4i <ilovegirlsinaglasses@gmail.com>
2023-09-23 02:36:20 +0300

    deleted prints

--- main.py
+++ main.py
@@ -2,25 +2,20 @@
 import	requests
 import csv
 from bs4 import BeautifulSoup, Tag
-import fake_useragent
 link = "https://icanhazip.com"
 vocubulary = []
 
-# verbs = ['sentir','calentar','empezar','despertar','mentir','perder','entendés','contar','costar','dormir','encontrar','volver','soñar','recordar','medir','reír','despedir','impedir','vestir','repetir','servir','elegir','seguir','jugar','traducir','salir','saber','dar','ver','estar','caber','huir','construir','sustituir','incluir','atribuir','traer','hacer','poner','conocer','conducir','parecer','ir']
-verbs = ['sentir', 'calentar']
+verbs = ['sentir','calentar','empezar','despertar','mentir','perder','entendés','contar','costar','dormir','encontrar','volver','soñar','recordar','medir','reír','despedir','impedir','vestir','repetir','servir','elegir','seguir','jugar','traducir','salir','saber','dar','ver','estar','caber','huir','construir','sustituir','incluir','atribuir','traer','hacer','poner','conocer','conducir','parecer','ir']
 
-# for dar in verbs:
-for i, dar in enumerate(verbs):    
-    link2 = f"https://www.wordreference.com/conj/esverbs.aspx?v={dar}"
+for i, verb in enumerate(verbs):    
+    link2 = f"https://www.wordreference.com/conj/esverbs.aspx?v={verb}"
     response = requests.get(link2).text
     soup = BeautifulSoup(response, 'lxml')
-    # print(soup)
     block = soup.find('table', id = "contenttable")
     allTables = block.find_all('table', class_ = "neoConj")
     indicativoPresente = allTables[0]
     imperativoAlfirmativo = allTables[15]
     imperativoNegativo = allTables[16]
-    # print(imperativoAlfirmativo.find('tbody'))
     columnSecond = ''
     for tr in imperativoAlfirmativo:
         firstColumn = tr.find('th')
@@ -28,11 +23,8 @@ for i, dar in enumerate(verbs):
         if type(firstColumn) == Tag and type(secondColumn) == Tag:
             secondColumnText = secondColumn.text.strip()
             if secondColumnText != "-" and secondColumnText != "–":
-                print(secondColumn.text)
-                # print(secondColumn.text)
                 columnSecond+="<div>" + firstColumn.text + " {{c1::" + secondColumn.text + "}}</div>"
-    vocubulary.append([dar, f"{columnSecond}"])
-print(vocubulary)
+    vocubulary.append([verb, f"{columnSecond}"])
 
 with open('imperativoAlfirmativo.csv', 'w', newline='') as file:
     writer = csv.writer(file, quoting=csv.QUOTE_MINIMAL, delimiter=';')
