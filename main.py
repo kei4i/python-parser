ea63420e96673f288795069450231ad33f4fef7d
kei4i <ilovegirlsinaglasses@gmail.com>
2023-09-25 00:06:30 +0300

    fixed verb

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup, Tag
 link = "https://icanhazip.com"
 vocubulary = []
 
-verbs = ['sentir','calentar','empezar','despertar','mentir','perder','entendés','contar','costar','dormir','encontrar','volver','soñar','recordar','medir','reír','despedir','impedir','vestir','repetir','servir','elegir','seguir','jugar','traducir','salir','saber','dar','ver','estar','caber','huir','construir','sustituir','incluir','atribuir','traer','hacer','poner','conocer','conducir','parecer','ir']
+verbs = ['sentir','calentar','empezar','despertar','mentir','perder','entender','contar','costar','dormir','encontrar','volver','soñar','recordar','medir','reír','despedir','impedir','vestir','repetir','servir','elegir','seguir','jugar','traducir','salir','saber','dar','ver','estar','caber','huir','construir','sustituir','incluir','atribuir','traer','hacer','poner','conocer','conducir','parecer','ir']
 
 for i, verb in enumerate(verbs):    
     link2 = f"https://www.wordreference.com/conj/esverbs.aspx?v={verb}"
@@ -17,7 +17,7 @@ for i, verb in enumerate(verbs):
     imperativoAlfirmativo = allTables[15]
     imperativoNegativo = allTables[16]
     columnSecond = ''
-    for tr in imperativoAlfirmativo:
+    for tr in indicativoPresente:
         firstColumn = tr.find('th')
         secondColumn = tr.find('td')
         if type(firstColumn) == Tag and type(secondColumn) == Tag:
@@ -25,7 +25,7 @@ for i, verb in enumerate(verbs):
             if secondColumnText != "-" and secondColumnText != "–":
                 columnSecond+="<div>" + firstColumn.text + " {{c1::" + secondColumn.text + "}}</div>"
     vocubulary.append([verb, f"{columnSecond}"])
-
-with open('imperativoAlfirmativo.csv', 'w', newline='') as file:
+print(vocubulary)
+with open('indicativoPresente.csv', 'w', newline='') as file:
     writer = csv.writer(file, quoting=csv.QUOTE_MINIMAL, delimiter=';')
     writer.writerows(vocubulary)
